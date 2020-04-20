983cfb52862ecc801849c253936a1e5264432e33
mmeqdar <58787227+mmeqdar@users.noreply.github.com>
2020-04-20 16:17:03 +0000

    DataBase

--- agriedge.sql
+++ agriedge.sql
@@ -2,10 +2,10 @@
 -- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 30, 2020 at 02:16 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.28
+-- Hôte : localhost
+-- Généré le : lun. 20 avr. 2020 à 18:15
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,17 +19,82 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `agriedge`
+-- Base de données : `agriedge`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `annonces`
+-- Structure de la table `annonces`
 --
 
 CREATE TABLE `annonces` (
   `id_annonce` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `id_categorie` int(11) NOT NULL,
+  `type_annonce` int(11) DEFAULT 0,
+  `id_coop` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Cart`
+--
+
+CREATE TABLE `Cart` (
+  `id_user` int(11) NOT NULL,
+  `id_annonce` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id_categorie` int(11) NOT NULL,
+  `name_categorie` varchar(255) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id_categorie`, `name_categorie`, `type`) VALUES
+(1, 'Pottery', 2),
+(2, 'Spices', 2),
+(3, 'Textile', 2),
+(4, 'Sewing and Embroidery', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `coop_ferme`
+--
+
+CREATE TABLE `coop_ferme` (
+  `id_cf` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `lat` varchar(255) NOT NULL,
+  `lng` varchar(255) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `demande`
+--
+
+CREATE TABLE `demande` (
+  `id_demande` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `description` varchar(255) NOT NULL,
   `quantity` int(11) NOT NULL,
@@ -40,27 +105,7 @@ CREATE TABLE `annonces` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categorie`
---
-
-CREATE TABLE `categorie` (
-  `id_categorie` int(11) NOT NULL,
-  `name_categorie` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `categorie`
---
-
-INSERT INTO `categorie` (`id_categorie`, `name_categorie`) VALUES
-(1, 'fruits'),
-(2, 'vegetables'),
-(3, 'cereal');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `images`
+-- Structure de la table `images`
 --
 
 CREATE TABLE `images` (
@@ -73,7 +118,7 @@ CREATE TABLE `images` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `regions`
+-- Structure de la table `regions`
 --
 
 CREATE TABLE `regions` (
@@ -82,7 +127,7 @@ CREATE TABLE `regions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `regions`
+-- Déchargement des données de la table `regions`
 --
 
 INSERT INTO `regions` (`id_region`, `name_region`) VALUES
@@ -102,7 +147,7 @@ INSERT INTO `regions` (`id_region`, `name_region`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
 CREATE TABLE `user` (
@@ -112,107 +157,151 @@ CREATE TABLE `user` (
   `password` varchar(1000) NOT NULL,
   `nexmo` varchar(255) DEFAULT NULL,
   `confirmation` int(11) NOT NULL DEFAULT 0,
-  `id_region` int(11) NOT NULL
+  `id_region` int(11) DEFAULT 1,
+  `type` int(11) NOT NULL DEFAULT 0,
+  `profil` varchar(255) NOT NULL DEFAULT 'default.jpg',
+  `lat` varchar(255) DEFAULT NULL,
+  `lng` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id_user`, `fullName`, `phone`, `password`, `nexmo`, `confirmation`, `id_region`) VALUES
-(88, 'test t', 699731346, '$2b$10$Ujc25bq2ulYEnpCoNRTb9efi0SltaQxQcLj0VpXatpotJa2t23t6O', '07f17933829c4ce880d9040b2458697f', 1, 5);
-
---
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `annonces`
+-- Index pour la table `annonces`
 --
 ALTER TABLE `annonces`
   ADD PRIMARY KEY (`id_annonce`),
   ADD KEY `categorie_fk` (`id_categorie`),
-  ADD KEY `user_fk` (`id_user`);
+  ADD KEY `user_fk` (`id_user`),
+  ADD KEY `coop_fk` (`id_coop`);
 
 --
--- Indexes for table `categorie`
+-- Index pour la table `Cart`
+--
+ALTER TABLE `Cart`
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_annonce` (`id_annonce`);
+
+--
+-- Index pour la table `categorie`
 --
 ALTER TABLE `categorie`
   ADD PRIMARY KEY (`id_categorie`);
 
 --
--- Indexes for table `images`
+-- Index pour la table `coop_ferme`
+--
+ALTER TABLE `coop_ferme`
+  ADD PRIMARY KEY (`id_cf`),
+  ADD KEY `id_usercf` (`id_user`);
+
+--
+-- Index pour la table `demande`
+--
+ALTER TABLE `demande`
+  ADD PRIMARY KEY (`id_demande`);
+
+--
+-- Index pour la table `images`
 --
 ALTER TABLE `images`
   ADD PRIMARY KEY (`Id_image`),
   ADD KEY `id_ann` (`id_annonce`);
 
 --
--- Indexes for table `regions`
+-- Index pour la table `regions`
 --
 ALTER TABLE `regions`
   ADD PRIMARY KEY (`id_region`);
 
 --
--- Indexes for table `user`
+-- Index pour la table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`),
   ADD KEY `id_reg` (`id_region`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `annonces`
+-- AUTO_INCREMENT pour la table `annonces`
 --
 ALTER TABLE `annonces`
-  MODIFY `id_annonce` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_annonce` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 
 --
--- AUTO_INCREMENT for table `categorie`
+-- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `images`
+-- AUTO_INCREMENT pour la table `coop_ferme`
+--
+ALTER TABLE `coop_ferme`
+  MODIFY `id_cf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT pour la table `demande`
+--
+ALTER TABLE `demande`
+  MODIFY `id_demande` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `images`
 --
 ALTER TABLE `images`
-  MODIFY `Id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
--- AUTO_INCREMENT for table `regions`
+-- AUTO_INCREMENT pour la table `regions`
 --
 ALTER TABLE `regions`
   MODIFY `id_region` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `annonces`
+-- Contraintes pour la table `annonces`
 --
 ALTER TABLE `annonces`
   ADD CONSTRAINT `categorie_fk` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `coop_fk` FOREIGN KEY (`id_coop`) REFERENCES `coop_ferme` (`id_cf`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user_fk` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `images`
+-- Contraintes pour la table `Cart`
+--
+ALTER TABLE `Cart`
+  ADD CONSTRAINT `id_annonce` FOREIGN KEY (`id_annonce`) REFERENCES `annonces` (`id_annonce`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `coop_ferme`
+--
+ALTER TABLE `coop_ferme`
+  ADD CONSTRAINT `id_usercf` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `images`
 --
 ALTER TABLE `images`
   ADD CONSTRAINT `id_ann` FOREIGN KEY (`id_annonce`) REFERENCES `annonces` (`id_annonce`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `user`
+-- Contraintes pour la table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `id_reg` FOREIGN KEY (`id_region`) REFERENCES `regions` (`id_region`) ON DELETE CASCADE ON UPDATE CASCADE;
